0bd2280cb3353a81b06dbc132dc89cdcfc203932
Akarsh Katiyar <92096960+Ak8933@users.noreply.github.com>
2024-08-17 01:42:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -106,6 +106,6 @@ app.delete('/banner/:id', (req, res) => {
 });
 
 // Start the server
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('Server is running on port 5000');
 });
